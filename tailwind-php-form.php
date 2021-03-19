f627226c0efdaeaea89d210f16089b5a82400d21
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2021-03-19 14:37:19 +0100

    Update tailwind-php-form.php

--- tailwind-php-form.php
+++ tailwind-php-form.php
@@ -18,17 +18,17 @@ function tailwind_php_form() {
 	$content .= '<form method="post" action="http://yourdomain.com/thank-you">';
 	$content .= '<div class="relative mb-4">
 	<label for="name" class="leading-7 text-sm text-gray-600">Name</label>
-	<input type="text" id="name" name="your_name" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+	<input type="text" id="name" name="your_name" required class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
   	</div>';
 
 	$content .= '<div class="relative mb-4">
 	<label for="email" class="leading-7 text-sm text-gray-600">Email</label>
-	<input type="email" id="email" name="your_email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+	<input type="email" id="email" name="your_email" required class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
   	</div>';
 
 	$content .= '<div class="relative mb-4">
 	<label for="message" class="leading-7 text-sm text-gray-600">Message</label>
-	<textarea id="message" name="your_comments" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+	<textarea id="message" name="your_comments" required class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
   	</div>';
 
 	$content .= '<input type="submit" name="example_form_submit" value="Submit" class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg cursor-pointer">';
